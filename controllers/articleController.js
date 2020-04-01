c426cf17232a3fb07bc1c41a4909129612efc167
Reuben Ayres <ra@sent.com>
2020-04-01 15:30:16 -0500

    using .populate() to replace author's id with actual author documents

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,6 +3,39 @@ const router = express.Router()
 const Author = require('../models/author.js')
 const Article = require('../models/article.js')
 
+
+// article index route: GET /articles
+router.get('/', (req, res, next) => {
+  // get the articles
+  // Article.find({}, (err, foundArticles) => {
+  //   if(err) next(err);
+  //   else {
+  //     // observe: a regular .find() on Articles gives us 
+  //     // article documents that just contain the Author's id (ObjectId)
+  //     console.log("\nhere are the articles we found");
+  //     console.log(foundArticles);
+  //     res.send('check terminal')
+  //   }
+  // })
+
+  // .populate(some path name that is a ref to an ID)
+  // this modifies the find query 
+  // for this path, go get the actual document for this id
+  // include the actual document in the query results instead of just the id
+
+  // so in this case it means "give me actual Author documents"
+  // instead of just IDs of Author documents
+  Article.find({}).populate('author').exec((err, foundArticles) => {
+    if(err) next(err);
+    else {
+      console.log("\nhere are the articles we found using .populate('author')");
+      console.log(foundArticles);
+      res.send('check terminal')
+    }
+  })
+}) // index route
+
+
 // article new route: GET /articles/new
 router.get('/new', (req, res, next) => {
   // we want to show a dropdown on the article new page
