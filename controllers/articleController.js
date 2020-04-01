0f112d142b62de870444c8e269ed07c94b38baee
Reuben Ayres <ra@sent.com>
2020-04-01 14:30:15 -0500

    article new page shows a drop down of authors to choose from

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author.js')
 
 // article new route: GET /articles/new
-router.get('/new', (req, res) => {
-  res.render('articles/new.ejs')
+router.get('/new', (req, res, next) => {
+  // we want to show a dropdown on the article new page
+  // to let user choose an author
+  // so we must query the db to get the authors
+  Author.find({}, (err, foundAuthors) => {
+    if(err) next(err);
+    else {
+      // be sure to make the authors available to the template!
+      res.render('articles/new.ejs', {
+        authors: foundAuthors
+      })
+    }
+  })
 })
 
 module.exports = router
